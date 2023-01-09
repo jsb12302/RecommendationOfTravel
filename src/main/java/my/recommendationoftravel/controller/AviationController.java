84153a46fccf939f9e8f2e27d2e13b9763d28c88
jsb12302 <jsb12302@naver.com>
2023-01-09 22:53:25 +0900

    feat : api 연동 및 뷰 페이지

--- src/main/java/my/recommendationoftravel/controller/AviationController.java
+++ src/main/java/my/recommendationoftravel/controller/AviationController.java
@@ -1,2 +1,31 @@
-package my.recommendationoftravel.controller;public class ActiationController {
+package my.recommendationoftravel.controller;
+
+import my.recommendationoftravel.domain.Country;
+import my.recommendationoftravel.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+public class AviationController {
+
+    private CountryService countryService;
+
+    @Autowired
+    public AviationController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @PostMapping("/getAviation")
+    public String countryListPage(@RequestParam String fromMonth, @RequestParam String toMonth, Model model) throws IOException, InterruptedException {
+        List<Country> countries = countryService.requestCountryApi(fromMonth, toMonth);
+        model.addAttribute("countryList", countries);
+        return "aviation/countryList";
+    }
+
 }
--- src/main/java/my/recommendationoftravel/domain/Country.java
+++ src/main/java/my/recommendationoftravel/domain/Country.java
@@ -1,2 +1,33 @@
-package my.recommendationoftravel.domain;public class countryt {
+package my.recommendationoftravel.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Country {
+
+    private String region;
+    private String country;
+    private int arrFlight;
+    private int depFlight;
+    private int flights;
+
+    public String getRegion() {
+        return region;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getArrFlight() {
+        return arrFlight;
+    }
+
+    public int getDepFlight() {
+        return depFlight;
+    }
+
+    public int getFlights() {
+        return flights;
+    }
 }
