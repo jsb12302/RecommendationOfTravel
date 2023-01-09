e2eeabc562ec4e056de0c2d723b861c5706954ca
jsb12302 <jsb12302@naver.com>
2023-01-09 17:33:46 +0900

    Initial commit

---
+++ src/main/java/my/recommendationoftravel/RecommendationOfTravelApplication.java
@@ -0,0 +1,13 @@
+package my.recommendationoftravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationOfTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendationOfTravelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/recommendationoftravel/RecommendationOfTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package my.recommendationoftravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationOfTravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
