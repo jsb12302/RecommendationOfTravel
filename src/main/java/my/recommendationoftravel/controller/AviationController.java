46891a81603466504a1ad9bc0bf504822a15abdb
jsb12302 <jsb12302@naver.com>
2023-01-10 18:10:34 +0900

    feat : validation 기능 추가

--- src/main/java/my/recommendationoftravel/controller/AviationController.java
+++ src/main/java/my/recommendationoftravel/controller/AviationController.java
@@ -1,13 +1,18 @@
 package my.recommendationoftravel.controller;
 
 import my.recommendationoftravel.domain.Country;
+import my.recommendationoftravel.domain.RequestDateDTO;
 import my.recommendationoftravel.service.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.io.IOException;
 import java.util.List;
 
@@ -22,8 +27,12 @@ public class AviationController {
     }
 
     @PostMapping("/getAviation")
-    public String countryListPage(@RequestParam String fromMonth, @RequestParam String toMonth, Model model) throws IOException, InterruptedException {
-        List<Country> countries = countryService.requestCountryApi(fromMonth, toMonth);
+    public String countryListPage(@Valid RequestDateDTO requestDateDTO, BindingResult bindingResult, Model model)
+            throws IOException, InterruptedException {
+        if(bindingResult.hasErrors()){
+            return "aviation/aviation";
+        }
+        List<Country> countries = countryService.requestCountryApi(requestDateDTO);
         model.addAttribute("countryList", countries);
         return "aviation/countryList";
     }
--- src/main/java/my/recommendationoftravel/domain/Country.java
+++ src/main/java/my/recommendationoftravel/domain/Country.java
@@ -2,9 +2,13 @@ package my.recommendationoftravel.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Builder
+@Getter
+@Setter
 public class Country {
 
     private String region;
