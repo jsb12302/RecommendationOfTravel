097987109a90e6216d5dec6dabd593e1b08f1677
jsb12302 <jsb12302@naver.com>
2023-01-14 17:50:24 +0900

    refactor : 코드 수정

--- src/main/java/my/recommendationoftravel/domain/Country.java
+++ src/main/java/my/recommendationoftravel/domain/Country.java
@@ -8,7 +8,6 @@ import lombok.Setter;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Builder
 @Getter
-@Setter
 public class Country {
 
     private String region;
@@ -16,19 +15,4 @@ public class Country {
     private int arrFlight;
     private int depFlight;
 
-    public String getRegion() {
-        return region;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public int getArrFlight() {
-        return arrFlight;
-    }
-
-    public int getDepFlight() {
-        return depFlight;
-    }
 }
