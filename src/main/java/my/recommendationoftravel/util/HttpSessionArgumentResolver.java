128346ffd3bd2fa0e4cfc92872952b24154330d2
jsb12302 <jsb12302@naver.com>
2023-01-27 19:41:21 +0900

    feat : HandlerMethodArgumentResolver 적용

--- src/main/java/my/recommendationoftravel/util/HttpSessionArgumentResolver.java
+++ src/main/java/my/recommendationoftravel/util/HttpSessionArgumentResolver.java
@@ -1,2 +1,30 @@
-package my.recommendationoftravel.util;public class HttpSessionArgumentResolver {
+package my.recommendationoftravel.util;
+
+import lombok.RequiredArgsConstructor;
+import my.recommendationoftravel.domain.user.User;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+@RequiredArgsConstructor
+@Component
+public class HttpSessionArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final HttpSession httpSession;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(SessionResolver.class) != null;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+       return httpSession.getAttribute("user");
+    }
 }
