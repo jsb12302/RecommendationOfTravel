4a6289e35a95da7a041418993d886c74540bcf94
jsb12302 <jsb12302@naver.com>
2023-01-30 01:06:43 +0900

    feat : 관리자 문의 사항 확인 기능 추가

--- src/main/java/my/recommendationoftravel/service/BoardService.java
+++ src/main/java/my/recommendationoftravel/service/BoardService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -45,5 +46,25 @@ public class BoardService {
         } else {
             throw new AlertException(ErrorMessage.NOT_FIND_BOARD);
         }
+    }
+
+    public Board adminGetBoard(Long boardId){
+        Optional<Board> board = boardRepository.findById(boardId);
+        return board.orElseThrow(
+                () ->  new AlertException(ErrorMessage.NOT_FIND_BOARD)
+        );
+    }
+
+    public List<Board> getAllBoards(){
+        return boardRepository.findAll();
+    }
+
+    @Transactional
+    public void postAnswer(String answer, Long boardId){
+        Optional<Board> boards = boardRepository.findById(boardId);
+        boards.ifPresentOrElse(
+                board -> board.setAnswer(answer),
+                () -> {throw new AlertException(ErrorMessage.NOT_FIND_BOARD);}
+        );
     }
 }
